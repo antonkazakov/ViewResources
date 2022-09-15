9584d61c3c6e13f94a8ca905622b3d5ad21563ba
Leonid <90571705+k1metsu@users.noreply.github.com>
2022-09-15 23:17:40 +0300

    second View

--- app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/CartActivity.kt
@@ -2,10 +2,18 @@ package otus.gpb.homework.viewandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.cart_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
