305151032ab2dbf35782989ffca42ca8a8d07116
paul <paul.bezborodov@yandex.ru>
2022-09-18 02:29:24 +0500

    Решение д.з. 04

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -2,10 +2,16 @@ package otus.gpb.homework.viewandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+        findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
