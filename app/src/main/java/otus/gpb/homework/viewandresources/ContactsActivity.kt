a3b231d213fa6c83df7e5b214e45a8eb9e476e0d
DmitryM <mikhaltchenkov@gmail.com>
2022-09-17 08:10:02 +0300

    added markup for contacts view

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -2,10 +2,18 @@ package otus.gpb.homework.viewandresources
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.contacts_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
