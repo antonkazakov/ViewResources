e336572c4d781e3b518b7e8ae5de13e634f0eb43
coolkosta <kostia.borzakov@gmail.com>
2022-09-20 22:41:10 +0300

    HW #4 Task - 1

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -1,11 +1,32 @@
 package otus.gpb.homework.viewandresources
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.EditText
+
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ContactsActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
+
+
