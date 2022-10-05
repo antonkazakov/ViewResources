4c89a5037ae2f248a70f2359c26e13a8fdf7f26b
coolkosta <kostia.borzakov@gmail.com>
2022-10-05 20:26:56 +0300

    Очистка пустых строк

--- app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
+++ app/src/main/java/otus/gpb/homework/viewandresources/ContactsActivity.kt
@@ -2,22 +2,15 @@ package otus.gpb.homework.viewandresources
 
 import android.os.Bundle
 import android.view.Menu
-import android.widget.EditText
 
 import androidx.appcompat.app.AppCompatActivity
 
-
 class ContactsActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-
 
     }
 
@@ -25,7 +18,6 @@ class ContactsActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.top_app_bar, menu)
         return super.onCreateOptionsMenu(menu)
     }
-
 
 }
 
